4b748788d5ca1fe559a1baadc88d9b08c4848121
Shubham Bagrecha <23572869+shu-bham@users.noreply.github.com>
2019-11-13 19:44:13 +0530

    added Logo, and SideDrawer in toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Auxilliary';
 import classes from './Layout.css';
-const layout = props => ( 
-    <Aux>
-        <div>Toolbar, Sidedrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+class Layout extends Component {
+    state = {
+        showSideDrawer : false
+    }
+
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer : false})
+    }
+ 
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+             return { showSideDrawer : !prevState.showSideDrawer}
+        });
+    }
+
+    render(){
+        return (
+            <Aux>
+                <Toolbar 
+                    open={this.state.showSideDrawer}
+                    drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer show={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );    
+    
+    }
+
+}
 
 
-export default layout;
+export default Layout;
